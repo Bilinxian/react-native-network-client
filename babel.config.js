a6f1844fe561adef3991dee988a060a125d05a77
wangzhi <wangzhi@cainiaomeishi.com>
2024-09-26 16:58:49 +0800

    还原1.3.8的库

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export * from "react-native";
-export {};
-//# sourceMappingURL=ReactNative.js.map
+module.exports = {
+    presets: ["module:metro-react-native-babel-preset"],
+};
