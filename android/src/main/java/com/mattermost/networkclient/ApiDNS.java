4f5baf48853e24816d795dbd09a027b4b66364bd
wangzhi <wangzhi0114@163.com>
2025-05-19 09:36:53 +0800

    暂时去除happy dns解析域名，使用系统解析优先，系统无法解析时，返回域名对应的固定ip

--- android/src/main/java/com/mattermost/networkclient/ApiDNS.java
+++ android/src/main/java/com/mattermost/networkclient/ApiDNS.java
@@ -4,76 +4,80 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.qiniu.android.dns.DnsManager;
-import com.qiniu.android.dns.IResolver;
-import com.qiniu.android.dns.NetworkInfo;
-import com.qiniu.android.dns.Record;
-import com.qiniu.android.dns.dns.DnsUdpResolver;
-
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import okhttp3.Dns;
 
 public class ApiDNS implements Dns {
 
-    private final DnsManager dns;
+//    private final DnsManager dns;
 
     public ApiDNS() {
-        IResolver[] resolvers = new IResolver[4];
-        resolvers[0] = new DnsUdpResolver("223.5.5.5"); //自定义 DNS 服务器地址
-        resolvers[1] = new DnsUdpResolver("119.29.29.29"); //自定义 DNS 服务器地址
-        resolvers[2] = new DnsUdpResolver("114.114.114.114"); //自定义 DNS 服务器地址
-        resolvers[3] = new DnsUdpResolver("180.76.76.76"); //自定义 DNS 服务器地址
-        dns = new DnsManager(NetworkInfo.normal, resolvers);
+//        IResolver[] resolvers = new IResolver[4];
+//        resolvers[0] = new DnsUdpResolver("223.5.5.5"); //自定义 DNS 服务器地址
+//        resolvers[1] = new DnsUdpResolver("119.29.29.29"); //自定义 DNS 服务器地址
+//        resolvers[2] = new DnsUdpResolver("114.114.114.114"); //自定义 DNS 服务器地址
+//        resolvers[3] = new DnsUdpResolver("180.76.76.76"); //自定义 DNS 服务器地址
+//        dns = new DnsManager(NetworkInfo.normal, resolvers);
 
     }
 
     @NonNull
     @Override
     public List<InetAddress> lookup(@NonNull String hostname) {
-        List<InetAddress> singletonList = new ArrayList<>();
-        try {
-            Record[] records = dns.queryRecords(hostname);
-            for (Record record : records) {
-                Log.i("TAG", "happy dns解析的地址：" + record);
-                singletonList.add(getAddress(hostname, record.value));
-            }
-            if (singletonList.isEmpty())
-                singletonList = getResultFail(hostname);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            singletonList = getResultFail(hostname);
-        }
+        List<InetAddress> singletonList  = getResultFail(hostname);
+//        try {
+//            Record[] records = dns.queryRecords(hostname);
+//            for (Record record : records) {
+//                Log.i("TAG", "happy dns解析的地址：" + record);
+//                singletonList.add(getAddress(hostname, record.value));
+//            }
+//            if (singletonList.isEmpty())
+//                singletonList = getResultFail(hostname);
+//
+//        } catch (IOException e) {
+//            singletonList = getResultFail(hostname);
+//        }
         Log.i("TAG", "最终dns解析的地址：" + singletonList);
         return singletonList;
     }
 
     private List<InetAddress> getResultFail(String hostname) {
         List<InetAddress> singletonList = new ArrayList<>();
+        InetAddress ipAddress;
         try {
             singletonList = Dns.SYSTEM.lookup(hostname);
         } catch (UnknownHostException e) {
-            e.printStackTrace();
+            switch (hostname) {
+                case "api.waisongbang.com":
+                    ipAddress = getAddress(hostname, "39.96.80.175");
+                    if (null != ipAddress)
+                        singletonList.add(ipAddress);
+                    break;
+                case "im.waisongbang.com":
+                case "yy.waisongbang.com":
+                case "product-service.waisongbang.com":
+                    ipAddress = getAddress(hostname, "47.93.170.207");
+                    if (null != ipAddress)
+                        singletonList.add(ipAddress);
+                    break;
+            }
         }
 
         return singletonList;
     }
 
-    private InetAddress getAddress(String hostname, String ip) throws UnknownHostException {
-        InetAddress ipAddress = InetAddress.getByName(ip);
-        Log.i("TAG", "ip获取：" + ipAddress);
-        byte[] ipBytes = ipAddress.getAddress();
-        Log.i("TAG", "网络地址字节：" + Arrays.toString(ipBytes));
-        InetAddress inetAddress = InetAddress.getByAddress(hostname, ipBytes);
-        Log.i("TAG", "网络地址：" + inetAddress);
-        return inetAddress;
+    private InetAddress getAddress(String hostname, String ip) {
+        try {
+            InetAddress ipAddress = InetAddress.getByName(ip);
+            byte[] ipBytes = ipAddress.getAddress();
+            return InetAddress.getByAddress(hostname, ipBytes);
+        } catch (UnknownHostException e) {
+            return null;
+        }
     }
 
 }
